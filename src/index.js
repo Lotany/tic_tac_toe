1838e547ff96bbdee863645fbabe71d5ccd9410f
Able <ablekilinda@gmail.com>
2022-11-15 15:05:00 +0300

    adding a prop

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ class Square extends React.Component{
     render() {
         return(<button className="square">
             {this.props.value}
-        </button>)
+        </button>);
     }
 }
+
