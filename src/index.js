a353467a16cf6005e29daeb42042dafcc13fa596
Able <ablekilinda@gmail.com>
2022-11-15 14:56:25 +0300

    adding values

--- src/index.js
+++ src/index.js
@@ -7,3 +7,11 @@ class Board extends React.Component {
         return <Square value={i} />;
     }
 }
+
+class Square extends React.Component{
+    render() {
+        return(<button className="square">
+            {this.props.value}
+        </button>)
+    }
+}
