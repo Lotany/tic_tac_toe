6977f291c12b5a1f959fa24de0d797854ce88964
Able <ablekilinda@gmail.com>
2022-11-15 14:51:05 +0300

    adding a board

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import './index.css';
 
+class Board extends React.Component {
+    renderSquare(i){
+        return <Square value={i} />;
+    }
+}
